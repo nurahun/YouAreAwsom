5df50d4fd4396f0227a511e77a2d1bdfa54c8701
nur <altun.darwaza@gmail.com>
2025-02-07 16:37:35 -0500

    Initial Commit

---
+++ YouAreAwsom/YouAreAwsomApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomApp.swift
+//  YouAreAwsom
+//
+//  Created by Nur Khadr on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
